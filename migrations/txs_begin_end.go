42c8c861f2474a00e674345fb71e2840f00b68e4
alex.sharov <AskAlexSharov@gmail.com>
2022-03-05 16:01:33 +0700

    save

--- migrations/txs_begin_end.go
+++ migrations/txs_begin_end.go
@@ -87,7 +87,7 @@ var txsBeginEnd2 = Migration{
 			case <-logEvery.C:
 				var m runtime.MemStats
 				runtime.ReadMemStats(&m)
-				log.Debug("[migration] Adding empty txs to begin/end of blocks",
+				log.Info("[migration] Adding empty txs to begin/end of blocks",
 					"processed", fmt.Sprintf("%.2f%%", 100-100*float64(blockNum)/float64(latestBlock)),
 					"alloc", common2.ByteCount(m.Alloc), "sys", common2.ByteCount(m.Sys))
 			default:
