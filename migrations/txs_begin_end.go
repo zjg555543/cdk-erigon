074f08739dacf832c4ec5598c2f1106a9522bab1
alex.sharov <AskAlexSharov@gmail.com>
2022-03-06 09:52:31 +0700

    save

--- migrations/txs_begin_end.go
+++ migrations/txs_begin_end.go
@@ -136,7 +136,6 @@ var txsBeginEnd2 = Migration{
 			if err = tx.Delete(kv.EthTx, numHashBuf, nil); err != nil {
 				return err
 			}
-			fmt.Printf("del: %d,%d\n", b.BaseTxId, b.BaseTxId+uint64(b.TxAmount)-1)
 			binary.BigEndian.PutUint64(numBuf, b.BaseTxId+uint64(b.TxAmount)-1) // del last tx in block
 			if err = tx.Delete(kv.EthTx, numHashBuf, nil); err != nil {
 				return err
@@ -150,11 +149,12 @@ var txsBeginEnd2 = Migration{
 				newBlock.Transactions, err = rawdb.CanonicalTransactions(tx, baseTxId, txAmount)
 				fmt.Printf("alex read: %d,%d, %d=%d\n", blockNum, baseTxId, txAmount, len(newBlock.Transactions))
 
-				for i, oldTx := range oldBlock.Transactions {
-					fmt.Printf("old: %d, %d\n", oldTx.GetNonce(), newBlock.Transactions[i].GetNonce())
-				}
+				//for i, oldTx := range oldBlock.Transactions {
+				//	fmt.Printf("old: %d, %d\n", oldTx.GetNonce(), newBlock.Transactions[i].GetNonce())
+				//}
 				for i, oldTx := range oldBlock.Transactions {
 					newTx := newBlock.Transactions[i]
+					fmt.Printf("found: %d\n", i)
 					if oldTx.GetNonce() != newTx.GetNonce() {
 						panic(blockNum)
 					}
@@ -263,7 +263,6 @@ func writeTransactionsNewDeprecated(db kv.RwTx, txs []types.Transaction, baseTxI
 			return fmt.Errorf("broken tx rlp: %w", err)
 		}
 
-		fmt.Printf("put: %d,%d\n", txId, tx.GetNonce())
 		// If next Append returns KeyExists error - it means you need to open transaction in App code before calling this func. Batch is also fine.
 		if err := db.Put(kv.EthTx, txIdKey, common.CopyBytes(buf.Bytes())); err != nil {
 			return err
