4d3930c2e00b0406ea12894efe85ecac03775bc2
alex.sharov <AskAlexSharov@gmail.com>
2022-03-24 09:46:37 +0700

    save

--- eth/stagedsync/stage_execute.go
+++ eth/stagedsync/stage_execute.go
@@ -271,7 +271,7 @@ Loop:
 		writeChangeSets := nextStagesExpectData || blockNum > cfg.prune.History.PruneTo(to)
 		writeReceipts := nextStagesExpectData || blockNum > cfg.prune.Receipts.PruneTo(to)
 		writeCallTraces := nextStagesExpectData || blockNum > cfg.prune.CallTraces.PruneTo(to)
-		fmt.Printf("prune: %d, %d=%t, %d=%t, %d=%t\n", blockNum, cfg.prune.History.PruneTo(to), writeChangeSets, cfg.prune.Receipts.PruneTo(to), writeReceipts, cfg.prune.CallTraces.PruneTo(to), writeCallTraces)
+		fmt.Printf("prune: %d, %d, %d=%t, %d=%t, %d=%t\n", nextStageProgress, blockNum, cfg.prune.History.PruneTo(to), writeChangeSets, cfg.prune.Receipts.PruneTo(to), writeReceipts, cfg.prune.CallTraces.PruneTo(to), writeCallTraces)
 		if err = executeBlock(block, tx, batch, cfg, *cfg.vmConfig, writeChangeSets, writeReceipts, writeCallTraces, contractHasTEVM, initialCycle); err != nil {
 			log.Error(fmt.Sprintf("[%s] Execution failed", logPrefix), "block", blockNum, "hash", block.Hash().String(), "err", err)
 			u.UnwindTo(blockNum-1, block.Hash())
