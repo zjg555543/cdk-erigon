a3528e57481db748fa038c75c7851ada7e402f91
alex.sharov <AskAlexSharov@gmail.com>
2022-04-01 10:14:06 +0700

    save

--- eth/stagedsync/stage_headers.go
+++ eth/stagedsync/stage_headers.go
@@ -967,10 +967,10 @@ func DownloadAndIndexSnapshotsIfNeed(s *StageState, ctx context.Context, tx kv.R
 		if err := cfg.snapshots.Reopen(); err != nil {
 			return fmt.Errorf("ReopenSegments: %w", err)
 		}
-		expect := cfg.snapshotHashesCfg.ExpectBlocks
-		if cfg.snapshots.SegmentsAvailable() < expect {
-			return fmt.Errorf("not enough snapshots available: %d > %d", expect, cfg.snapshots.SegmentsAvailable())
-		}
+		//expect := cfg.snapshotHashesCfg.ExpectBlocks
+		//if cfg.snapshots.SegmentsAvailable() < expect {
+		//	return fmt.Errorf("not enough snapshots available: %d > %d", expect, cfg.snapshots.SegmentsAvailable())
+		//}
 		if err := cfg.snapshots.Reopen(); err != nil {
 			return fmt.Errorf("ReopenIndices: %w", err)
 		}
