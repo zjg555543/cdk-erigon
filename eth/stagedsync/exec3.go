438ad8592f9143dba240b8bcc1700416449955c3
alex.sharov <AskAlexSharov@gmail.com>
2023-01-08 13:53:11 +0700

    save

--- eth/stagedsync/exec3.go
+++ eth/stagedsync/exec3.go
@@ -514,7 +514,7 @@ Loop:
 				Coinbase:     b.Coinbase(),
 				Uncles:       b.Uncles(),
 				Rules:        rules,
-				Txs:          txs,
+				Txs:          b.Copy().Transactions(),
 				TxNum:        inputTxNum,
 				TxIndex:      txIndex,
 				BlockHash:    b.Hash(),
