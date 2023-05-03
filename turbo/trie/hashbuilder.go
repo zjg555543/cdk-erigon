108078a30e242b3241c30d0e3ffd5e0d3534f2ec
alex.sharov <AskAlexSharov@gmail.com>
2023-05-03 10:30:01 +0700

    save

--- turbo/trie/hashbuilder.go
+++ turbo/trie/hashbuilder.go
@@ -357,7 +357,7 @@ func (hb *HashBuilder) accountLeafHashWithKey(key []byte, popped int) error {
 		hb.hashStack = hb.hashStack[:len(hb.hashStack)-popped*hashStackStride]
 		hb.nodeStack = hb.nodeStack[:len(hb.nodeStack)-popped]
 	}
-	//fmt.Printf("accountLeafHashWithKey [%x]=>[%x]\nHash [%x]\n", key, val, hb.hashBuf[:])
+	fmt.Printf("accountLeafHashWithKey [%x]=>[%x]\nHash [%x]\n", key, hb.valBuf[:valLen], hb.hashBuf[:])
 	hb.hashStack = append(hb.hashStack, hb.hashBuf[:]...)
 	hb.nodeStack = append(hb.nodeStack, nil)
 	if hb.trace {
