70103d3f00a63cb443c5416b89bccefabb258ac4
alex.sharov <AskAlexSharov@gmail.com>
2023-05-09 10:06:59 +0700

    save

--- core/state/plain_state_writer.go
+++ core/state/plain_state_writer.go
@@ -77,6 +77,7 @@ func (w *PlainStateWriter) UpdateAccountCode(address libcommon.Address, incarnat
 }
 
 func (w *PlainStateWriter) DeleteAccount(address libcommon.Address, original *accounts.Account) error {
+	fmt.Printf("DeleteAccount: %x\n", address)
 	//fmt.Printf("delete,%x\n", address)
 	if w.csw != nil {
 		if err := w.csw.DeleteAccount(address, original); err != nil {
