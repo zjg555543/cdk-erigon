871b55dc3771190b29eecd7dd4ad9704c7941c06
alex.sharov <AskAlexSharov@gmail.com>
2023-06-30 09:40:12 +0700

    save

--- cmd/rpcdaemon/commands/debug_api_test.go
+++ cmd/rpcdaemon/commands/debug_api_test.go
@@ -158,7 +158,6 @@ func TestTraceTransaction(t *testing.T) {
 }
 
 func TestTraceTransactionNoRefund(t *testing.T) {
-	t.Skip()
 	m, _, _ := rpcdaemontest.CreateTestSentry(t)
 	api := NewPrivateDebugAPI(newBaseApiForTest(m), m.DB, 0)
 	for _, tt := range debugTraceTransactionNoRefundTests {
