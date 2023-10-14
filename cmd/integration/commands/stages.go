b3d1f4616782622e7b28911091e14299a33eaec2
alex.sharov <AskAlexSharov@gmail.com>
2023-10-14 09:15:19 +0700

    save

--- cmd/integration/commands/stages.go
+++ cmd/integration/commands/stages.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"math"
+	"os"
 	"strings"
 	"sync"
 	"time"
@@ -13,6 +14,7 @@ import (
 	"github.com/c2h5oh/datasize"
 	"github.com/ledgerwatch/log/v3"
 	"github.com/ledgerwatch/secp256k1"
+	"github.com/shirou/gopsutil/v3/process"
 	"github.com/spf13/cobra"
 	"golang.org/x/exp/slices"
 
@@ -1450,7 +1452,51 @@ func stageTxLookup(db kv.RwDB, ctx context.Context, logger log.Logger) error {
 	return tx.Commit()
 }
 
+var rlimit2str = map[int32]string{
+	process.RLIMIT_CPU:        "cpu",
+	process.RLIMIT_FSIZE:      "fsize",
+	process.RLIMIT_DATA:       "data",
+	process.RLIMIT_STACK:      "stack",
+	process.RLIMIT_CORE:       "core",
+	process.RLIMIT_RSS:        "rss",
+	process.RLIMIT_NPROC:      "nproc",
+	process.RLIMIT_NOFILE:     "nofile",
+	process.RLIMIT_MEMLOCK:    "memlock",
+	process.RLIMIT_AS:         "as",
+	process.RLIMIT_LOCKS:      "locks",
+	process.RLIMIT_SIGPENDING: "sigpending",
+	process.RLIMIT_MSGQUEUE:   "msgqueue",
+	process.RLIMIT_NICE:       "nice",
+	process.RLIMIT_RTPRIO:     "prtrio",
+	process.RLIMIT_RTTIME:     "rttime",
+}
+
+func printRlimit() error {
+	checkPid := os.Getpid()
+	pr, err := process.NewProcess(int32(checkPid))
+	if err != nil {
+		return err
+	}
+	rl, err := pr.RlimitUsage(true)
+	if err != nil {
+		return err
+	}
+	for _, rll := range rl {
+		if rll.Used < 1000 {
+			continue
+		}
+		//switch rll.Resource {
+		//case process.RLIMIT_RSS, process.RLIMIT_RSS:
+		fmt.Printf("%s: hard=%s, soft=%s, used=%s, %d%%\n", rlimit2str[rll.Resource], datasize.ByteSize(rll.Hard).String(), datasize.ByteSize(rll.Soft).String(), datasize.ByteSize(rll.Used).String(), rll.Used/rll.Soft)
+		//default:
+		//}
+	}
+	return nil
+}
 func printAllStages(db kv.RoDB, ctx context.Context, logger log.Logger) error {
+	if err := printRlimit(); err != nil {
+		return err
+	}
 	sn, borSn, agg := allSnapshots(ctx, db, logger)
 	defer sn.Close()
 	defer borSn.Close()
