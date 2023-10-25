bf2356661a098d68f73e8231fd4e0312ce2b65d4
alex.sharov <AskAlexSharov@gmail.com>
2023-10-25 12:03:41 +0700

    save

--- eth/stagedsync/sync.go
+++ eth/stagedsync/sync.go
@@ -108,6 +108,7 @@ func (s *Sync) IsAfter(stage1, stage2 stages.SyncStage) bool {
 }
 
 func (s *Sync) UnwindTo(unwindPoint uint64, reason UnwindReason) {
+	fmt.Printf("[dbg] UnwindTo %d, %s\n", unwindPoint, dbg.Stack())
 	if reason.Block != nil {
 		s.logger.Debug("UnwindTo", "block", unwindPoint, "block_hash", reason.Block.String(), "err", reason.Err)
 	} else {
