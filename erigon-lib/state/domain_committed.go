2169d7507efa95b258754c9819bcdfe083c8107c
alex.sharov <AskAlexSharov@gmail.com>
2023-10-25 13:19:22 +0700

    save

--- erigon-lib/state/domain_committed.go
+++ erigon-lib/state/domain_committed.go
@@ -546,9 +546,9 @@ func (d *DomainCommitted) SeekCommitment(tx kv.Tx, sinceTx, untilTx uint64, cd *
 		return 0, 0, fmt.Errorf("state storing is only supported hex patricia trie")
 	}
 
-	if d.trace {
-		fmt.Printf("[commitment] SeekCommitment [%d, %d]\n", sinceTx, untilTx)
-	}
+	//if d.trace {
+	d.logger.Warn("[snapshots] SeekCommitment", "sinceTx", sinceTx, "untilTx", untilTx)
+	//}
 
 	var latestState []byte
 	err = cd.IteratePrefix(tx, keyCommitmentState, func(key, value []byte) error {
@@ -556,9 +556,9 @@ func (d *DomainCommitted) SeekCommitment(tx kv.Tx, sinceTx, untilTx uint64, cd *
 			return fmt.Errorf("invalid state value size %d [%x]", len(value), value)
 		}
 		txn, bn := binary.BigEndian.Uint64(value), binary.BigEndian.Uint64(value[8:16])
-		if d.trace {
-			fmt.Printf("[commitment] Seek found committed txn %d block %d\n", txn, bn)
-		}
+		//if d.trace {
+		d.logger.Warn("[snapshots] SeekCommitment", "txn", txn, "block", bn)
+		//}
 
 		if txn >= sinceTx && txn <= untilTx {
 			latestState = value
