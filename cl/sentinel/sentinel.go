1614fb0283bc7cbb9d4ccc9585ae453e593f299a
alex.sharov <AskAlexSharov@gmail.com>
2023-10-31 11:30:26 +0700

    save

--- cl/sentinel/sentinel.go
+++ cl/sentinel/sentinel.go
@@ -228,6 +228,13 @@ func New(
 	//}
 
 	limits := rcmgr.DefaultLimits
+	limits.SystemBaseLimit.Conns = 128               // 128
+	limits.SystemBaseLimit.ConnsInbound = 64         // 64
+	limits.SystemBaseLimit.ConnsOutbound = 64        // 128
+	limits.SystemBaseLimit.Streams = 128 * 16        // 128 * 16
+	limits.SystemBaseLimit.StreamsInbound = 64 * 16  // 64 * 16
+	limits.SystemBaseLimit.StreamsOutbound = 64 * 16 // 128 * 16
+
 	// LimitIncrease is the additional limit granted for every additional 1 GB of RAM.
 	limits.SystemLimitIncrease.Conns = 4           // 128
 	limits.SystemLimitIncrease.ConnsInbound = 2    // 64
