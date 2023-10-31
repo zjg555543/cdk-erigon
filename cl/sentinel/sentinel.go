52f29eb6d8885326c0fb9a04fa3bcf3a294c7e68
alex.sharov <AskAlexSharov@gmail.com>
2023-10-31 11:08:57 +0700

    save

--- cl/sentinel/sentinel.go
+++ cl/sentinel/sentinel.go
@@ -230,12 +230,12 @@ func New(
 	}
 
 	limits := rcmgr.DefaultLimits
-	limits.SystemLimitIncrease.Conns = 8            // 128
-	limits.StreamLimitIncrease.ConnsInbound = 4     // 64
-	limits.StreamLimitIncrease.ConnsOutbound = 4    // 128
-	limits.SystemLimitIncrease.Streams = 32         // 128*16
-	limits.StreamLimitIncrease.StreamsInbound = 16  // 128*16
-	limits.SystemLimitIncrease.StreamsOutbound = 16 // 128*16
+	limits.SystemLimitIncrease.Conns = 8           // 128
+	limits.StreamLimitIncrease.ConnsInbound = 4    // 64
+	limits.StreamLimitIncrease.ConnsOutbound = 4   // 128
+	limits.SystemLimitIncrease.Streams = 16        // 128*16
+	limits.StreamLimitIncrease.StreamsInbound = 8  // 128*16
+	limits.SystemLimitIncrease.StreamsOutbound = 8 // 128*16
 
 	limits.TransientLimitIncrease.Conns /= reduce
 	limits.TransientLimitIncrease.ConnsInbound /= reduce
